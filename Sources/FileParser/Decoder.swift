153e02894eb78a3f830e8980043bb1ebe7eb906e
Whitney Foster <whitney.m.foster@gmail.com>
2021-08-29 12:28:07 -0700

    Decode object from file and other file reading and parsing tools

---
+++ Sources/FileParser/Decoder.swift
@@ -0,0 +1,19 @@
+//
+//  Decoder.swift
+//  
+//
+//  Created by Whitney Foster on 8/29/21.
+//
+
+import Foundation
+
+public struct Decoder {
+    public func decodeObjectFromFile<T: Decodable>(objectType: T.Type, fromFile filePath: String) throws -> T {
+        return try self.decodeObjectFromJSONData(objectType: objectType,
+                                                 fromData: try FileParser().readJSONDataFromFile(filePath))
+    }
+    
+    public func decodeObjectFromJSONData<T: Decodable>(objectType: T.Type, fromData data: Data) throws -> T {
+        return try JSONDecoder().decode(objectType, from: data)
+    }
+}
--- Sources/FileParser/FileParser.swift
+++ Sources/FileParser/FileParser.swift
@@ -1,5 +1,32 @@
+import Foundation
+
 public struct FileParser {
-    public var text = "Hello, World!"
+    public func readJSONDataFromFile(_ filePath: String) throws -> Data {
+        return try self.readDataFromPath(bundlePath: self.bundlePath(forResource: filePath, ofType: "json"))
+    }
+}
+
+// ============ //
+
+private extension FileParser {
+    enum ParseError: Error {
+        case bundlePathForResourceNotFound
+        case dataCouldNotBeRetreivedFromFile
+    }
+}
+
+private extension FileParser {
+    func bundlePath(forResource resource: String, ofType type: String) throws -> String {
+        guard let path = Bundle.main.path(forResource:resource, ofType: type) else {
+            throw ParseError.bundlePathForResourceNotFound
+        }
+        return path
+    }
     
-    public init() {}
+    func readDataFromPath(bundlePath: String) throws -> Data {
+        guard let data = try String(contentsOfFile: bundlePath).data(using: .utf8) else {
+            throw ParseError.dataCouldNotBeRetreivedFromFile
+        }
+        return data
+    }
 }
